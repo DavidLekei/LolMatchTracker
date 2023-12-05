bded14876e244a192512e86ca9c0642fb343d3aa
DAVID <david.w.lekei@gmail.com>
2023-12-04 23:52:08 -0600

    Added Progress Bar that increments as the video plays. Removed Full Volume icon

--- WebApp/lol-match-tracker/src/Components/Video/VideoControls.jsx
+++ WebApp/lol-match-tracker/src/Components/Video/VideoControls.jsx
@@ -17,6 +17,13 @@ import './Video.css'
 export default function VideoControls(props){
 
 	const [volume, setVolume] = useState(30);
+	const [progress, setProgress] = useState(0)
+	// const [duration, setDuration] = useState([0, 100])
+
+	// const handleDrag = (event, newValue) => {
+	// 	console.log('when does this get called? ', newValue)
+	// 	setProgress(newValue)
+	// }
 
 	const [isFullscreen, setIsFullscreen] = useState(false)
 
@@ -43,6 +50,7 @@ export default function VideoControls(props){
 	}
 
 	const mute = () => {
+		console.log('mute')
 		getVideo().volume = 0.0
 		setVolume(0)
 	}
@@ -64,33 +72,59 @@ export default function VideoControls(props){
 		setIsFullscreen(false)
 	}
 
+	// const updateProgress = () => {
+	// 	// setProgress(progress + 4)
+	// }
+
+	const seek = (event, newValue) => {
+		console.log('seek')
+		setProgress(newValue)
+		console.log('setProgress called?')
+	}
+
+	const addAnnotation = () => {
+
+	}
+
 	useEffect(() => {
 		document.getElementById('video-player-overlay').addEventListener("mouseenter", (event) => {
-			document.getElementById('video-controls').className = "video-controls"
+			document.getElementById('video-controls').className = ""
 		})
 
 		document.getElementById('video-player-overlay').addEventListener("mouseleave", (event) => {
-			document.getElementById('video-controls').className = "video-controls hidden"
+			document.getElementById('video-controls').className = "hidden"
+		})
+
+		getVideo().addEventListener('timeupdate', (event) => {
+			const currentTime = getVideo().currentTime
+			const duration = getVideo().duration
+			const progressBar = document.getElementById('progress-bar')
+			progressBar.value = (currentTime / duration) * 100
+			console.log('value: ', progressBar.value)
 		})
 	}, [])
 
 	return(
-		<div id="video-controls" className="video-controls">
-			<div className="control-group playback-controls">
-				<PlayArrowIcon className="video-player-icon" onClick={play}/>
-				<PauseOutlinedIcon className="video-player-icon" onClick={pause}/>
-				<StopIcon className="video-player-icon" onClick={stop}/>
+		<div id="video-controls">
+			<div className="progress-controls">
+				<input id="progress-bar" type="range" value="0" min="0" max="100" step="0.25"/>
 			</div>
-			<div className="control-group  other-controls">
-				<TextSnippetOutlinedIcon className="video-player-icon" />
-				<Box sx={{ width: 200 }}>
-					<Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
-					  <VolumeDown id="volume-down" onClick={mute}/>
-					  <Slider aria-label="Volume" value={volume} onChange={handleVolumeChange} />
-					  <VolumeUp id="volume-up" onClick={max}/>
-					</Stack>
-				</Box>
-				{isFullscreen ? <FullscreenExitIcon id="fullscreenexit-icon" className="video-player-icon" onClick={exitFullscreen} /> : <FullscreenIcon id="fullscreen-icon" className="video-player-icon" onClick={fullscreen} />}
+			<div className="video-controls">
+				<div className="control-group playback-controls">
+					<PlayArrowIcon className="video-player-icon" onClick={play}/>
+					<PauseOutlinedIcon className="video-player-icon" onClick={pause}/>
+					<StopIcon className="video-player-icon" onClick={stop}/>
+				</div>
+				<div className="control-group  other-controls">
+					<TextSnippetOutlinedIcon className="video-player-icon" onClick={addAnnotation}/>
+					<Box sx={{ width: 200 }}>
+						<Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
+						  <VolumeDown id="volume-down" className="video-player-icon" onClick={mute}/>
+						  <Slider aria-label="Volume" value={volume} onChange={handleVolumeChange} />
+						</Stack>
+					</Box>
+					{isFullscreen ? <FullscreenExitIcon id="fullscreenexit-icon" className="video-player-icon" onClick={exitFullscreen} /> : <FullscreenIcon id="fullscreen-icon" className="video-player-icon" onClick={fullscreen} />}
+				</div>
 			</div>
 		</div>
 	)
