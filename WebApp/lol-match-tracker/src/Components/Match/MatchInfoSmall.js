2f269523ac2ceb39e3eda23ba3a52ce12c6d0461
DAVID <david.w.lekei@gmail.com>
2024-01-03 20:16:06 -0600

    Visual updates

--- WebApp/lol-match-tracker/src/Components/Match/MatchInfoSmall.js
+++ WebApp/lol-match-tracker/src/Components/Match/MatchInfoSmall.js
@@ -9,58 +9,69 @@ import Button from '@mui/material/Button'
 import './MatchInfo.css'
 import './MatchesListview.css'
 
+function ChampionInfo(props){
+
+    console.log('championinfo props: ', props)
+
+    const summs = props.summoners.map((summ, index) => {
+        console.log('summ : ', summ)
+        return <img className="match-info-img" src={`/game/summoners/${summ}.png`} />
+    })
+
+    const runes = props.runes.map((rune, index) => {
+        return <img className="match-info-img" src={`/game/perk-images/Styles/${rune}.png`} />
+    })
+
+    const items = props.items.map((item, index) => {
+        return <img className="match-info-img" src={`/game/item/${item}.png`} />
+    })
+
+    return(
+        <div className="column champion-info">
+            <div className="row">
+                <img className="match-info-img champ-img" src={`/game/champion/${props.name}.png`} />
+                <div className="column">
+                    {summs}
+                </div>
+                <div className="column">
+                    {runes}
+                </div>
+            </div>
+            <div className="row items">
+                {items}
+            </div>
+        </div>
+    )
+}
+
 export default function MatchInfo(props){
 
     console.log('matchinfo props: ', props)
 
     return(
-        <div className="match-info-container">
-            <div className="row" style={{width:'30%', outline:'2px solid blue'}}>
-                <div className="column champion-info">
-                    <div className="row">
-                        <img className="match-info-img champ-img" src={`/game/champion/${props.data.championPlayed.name}.png`} />
-                        <div className="column">
-                            <img className="match-info-img" src={`/game/summoners/flash.png`} />
-                            <img className="match-info-img" src={`/game/summoners/teleport.png`} />
-                        </div>
-                    </div>
-                    <div className="row items">
-                        <img className="match-info-img" src="/game/item/3070.png" />
-                        <img className="match-info-img" src="/game/item/3070.png" />
-                        <img className="match-info-img" src="/game/item/3070.png" />
-                        <img className="match-info-img" src="/game/item/3070.png" />
-                        <img className="match-info-img" src="/game/item/3070.png" />
-                        <img className="match-info-img" src="/game/item/3070.png" />
-                    </div>
-                </div>
-                <div className="column">
-                    <div className="row">
-                        <img className="match-info-img champ-img" src={`/game/champion/${props.data.championAgainst.name}.png`} />
-                        <div className="column">
-                            <img className="match-info-img" src={`/game/summoners/flash.png`} />
-                            <img className="match-info-img" src={`/game/summoners/teleport.png`} />
-                        </div>
-                    </div>
-                    <div className="row items">
-                        <img className="match-info-img" src="/game/item/3070.png" />
-                        <img className="match-info-img" src="/game/item/3070.png" />
-                        <img className="match-info-img" src="/game/item/3070.png" />
-                        <img className="match-info-img" src="/game/item/3070.png" />
-                        <img className="match-info-img" src="/game/item/3070.png" />
-                        <img className="match-info-img" src="/game/item/3070.png" />
-                    </div>
+        <div className={`match-info-container ${props.data.outcome}`}>
+            <div className="row" style={{width:'30%'}}>
+                <ChampionInfo name={props.data.championPlayed.name} summoners={['flash', 'teleport']} runes={['7200_Domination', '7202_Sorcery']} items={[3070, 3157, 6653, 3135, 3089]} />
+                <ChampionInfo name={props.data.championAgainst.name} summoners={['flash', 'ignite']} runes={['7202_Sorcery', '7203_Whimsy']} items={[6655, 3135, 4629, 3102, 3089, 3040]} />
+            </div>
+            <div style={{width:'30%'}}>
+                <div className="column la bold" id="match-stats">
+                    <div id="kda">{props.data.kda}</div>
+                    <div>CS: 280</div>
+                    <div>Vision: 22</div>
                 </div>
             </div>
-            <div style={{width:'30%', outline:'2px solid blue'}}>
-                Statistics
-            </div>
-            <div className="row" style={{width:'30%', outline:'2px solid blue'}}>
+            <div className="row space-apart" style={{width:'30%'}}>
                 <div>
                     Other players
                 </div>
                 <div className="column">
-                    <Button>Link to Recording</Button>
-                    <Button>Link to Notes</Button>
+                    <Link className="match-info-icon" to={`/recordings/${props.data.id}`}>
+                        <img className="match-info-img" src="/icons/video.png" />
+                    </Link>
+                    <Link className="match-info-icon" to="/notes">
+                        <img className="match-info-img" src="/icons/notes.png" />
+                    </Link>
                 </div>
             </div>
         </div>
